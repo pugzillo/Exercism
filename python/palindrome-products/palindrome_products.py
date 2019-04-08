eaf55ce0a7133596d03f1e8511e8d33c9006fa8c
Linh <lcahu91@gmail.com>
2019-04-08 08:24:21 -0500

    WIP

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -1,11 +1,3 @@
-def get_factors(x):
-    factors = list()
-    for i in range(1, x+1):
-       if x % i == 0:
-           factors.append(i)
-    return(factors)
-
-
 def get_factors_in_range(x,min_factor, max_factor):
     factors = list()
     for i in range(min_factor, max_factor+1):
@@ -14,6 +6,9 @@ def get_factors_in_range(x,min_factor, max_factor):
     return(factors)
 
 def get_paired_factors_in_range(num, min_factor, max_factor):
+    """
+    Two numbers that multiply to a specific number (ie. 2 and 3 are factors of 6. Also 1 and 6)
+    """
     paired_factors = set()
     # get_factors to get all od the factors
     factors = get_factors_in_range(num, min_factor, max_factor)
@@ -25,27 +20,37 @@ def get_paired_factors_in_range(num, min_factor, max_factor):
                 paired_factors.add((factor_a, factor_b))
     return(paired_factors)
 
+def get_extreme_palindrome(max_factor, min_factor, min_or_max):
+        min_product = min_factor ** 2
+        max_product = max_factor ** 2
+        # Find the minimum palindrone
+        # go between th min and max product
+        range_args = [
+            min_product,
+            max_product + 1
+        ] if min_or_max is 'min' else [
+            max_product,
+            min_product - 1,
+            -1
+        ]
+        for candidate_for_extreme_palindrome in range(*range_args):
+            # if the number id not a palindrone, then next
+            if str(candidate_for_extreme_palindrome)[::-1] != str(candidate_for_extreme_palindrome):
+                continue
+            # if the number is a palindrone, then we'll see if we can get a paired factor within range
+            # get the products for i and then use a doulble loop to find paired factors
+            factors = get_factors_in_range(candidate_for_extreme_palindrome,min_factor, max_factor)
+            for factor_a in factors:
+                for factor_b in factors:
+                    product = factor_a * factor_b
+                    if product == candidate_for_extreme_palindrome:
+                        return(candidate_for_extreme_palindrome)
+
 
 def smallest_palindrome(max_factor, min_factor):
     if min_factor > max_factor:
         raise ValueError("Min greater than max")
-    def get_smallest_palindrome(max_factor, min_factor):
-        min_product = min_factor ** 2
-        max_product = max_factor ** 2
-        # Find the minimum palindrone
-        # go between th min and max product 
-        for i in range(min_product,max_product+1):
-            # if the number id not a palindrone, then next
-            if str(i)[::-1] != str(i):
-                continue
-            # if the number is a palindrone, then we'll see if we can get a paired factor within range
-            # get the products for i and then use a doulble loop to find paired factors
-            for factor_a in get_factors_in_range(i,min_factor, max_factor):
-                for factor_b in get_factors_in_range(i,min_factor, max_factor):
-                    product = factor_a * factor_b
-                    if product == i:
-                        return(i)
-    smallest_palindrome = get_smallest_palindrome(max_factor, min_factor)
+    smallest_palindrome = get_extreme_palindrome(max_factor, min_factor, "min")
     if smallest_palindrome is None:
         return([None, list()])
     product_pairs = get_paired_factors_in_range(smallest_palindrome, min_factor, max_factor)
@@ -54,23 +59,7 @@ def smallest_palindrome(max_factor, min_factor):
 def largest_palindrome(max_factor, min_factor):
     if min_factor > max_factor:
         raise ValueError("Min greater than max")
-    def get_largest_palindrome(max_factor, min_factor):
-        min_product = min_factor ** 2
-        max_product = max_factor ** 2
-        # Find the minimum palindrone
-        # go between th min and max product 
-        for i in range(max_product,min_product-1, -1):
-            # if the number id not a palindrone, then next
-            if str(i)[::-1] != str(i):
-                continue
-            # if the number is a palindrone, then we'll see if we can get a paired factor within range
-            # get the products for i and then use a doulble loop to find paired factors
-            for factor_a in get_factors_in_range(i,min_factor, max_factor):
-                for factor_b in get_factors_in_range(i,min_factor, max_factor):
-                    product = factor_a * factor_b
-                    if product == i:
-                        return(i)
-    largest_palindrome = get_largest_palindrome(max_factor, min_factor)
+    largest_palindrome = get_extreme_palindrome(max_factor, min_factor, "max")
     if largest_palindrome is None:
         return([None, list()])
     product_pairs = get_paired_factors_in_range(largest_palindrome, min_factor, max_factor)
