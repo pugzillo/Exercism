4ee0894579ea6204eef18a3f9153fa7d5b79db04
Linh <lcahu91@gmail.com>
2019-05-13 20:39:10 -0500

    Added List Comprehension

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,10 +2,11 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("DNA strands don't have matching lengths!")
     
-    hamming = 0
+    # hamming = sum(tpl[0] != tpl[1] for tpl in zip(strand_a, strand_b))
 
+    hamming = 0
     for i in range(len(strand_a)):
-        if list(strand_a)[i] != list(strand_b)[i]:
+        if strand_a[i] != strand_b[i]:
             hamming += 1 
     
     return(hamming)
